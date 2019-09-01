22a053cedab4ffc4dd6db988895ff764c06f5a52
apate107 <apate107@stevens.edu>
2019-09-01 00:38:41 -0400

    Added plot features and updated README

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 ##### I pledge my honor that I have abided by the Stevens Honor System.
 
 This script creates a sine and cosine series over one period using `numpy` and graphs both on the same axis using `matplotlib`.
+Below is the resulting graph from running the script:
+
+![Graph of sine and cosine](https://github.com/apate107/fe595_1-python-refresher/raw/master/Figure_1.png)
--- refresher.py
+++ refresher.py
@@ -10,7 +10,11 @@ if __name__ == '__main__':
     cos2 = np.cos(x)
 
     # Then, plot both series on the same axis
-    plt.plot(x, sin2)
-    plt.plot(x, cos2)
+    plt.plot(x, sin2, 'r-', label='sin')
+    plt.plot(x, cos2, 'b-', label='cos')
     plt.xticks([0, np.pi/2, np.pi, (3*np.pi)/2, 2*np.pi], ['0', 'pi/2', 'pi', '3pi/2', '2pi'])
+    plt.xlabel('Angle (in radians)')
+    plt.ylabel('Value')
+    plt.title('Sine and cosine over one period')
+    plt.legend(loc=3)
     plt.show()
