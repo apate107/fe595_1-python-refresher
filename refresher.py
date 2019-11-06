fc9b15c38852b4d71abdf8349544d0a8a8a60478
apate107 <apate107@stevens.edu>
2019-11-06 13:23:11 -0500

    Add tan
    Did this on my own repo because two others in my group used each others and already merged the changes

--- refresher.py
+++ refresher.py
@@ -8,13 +8,20 @@ if __name__ == '__main__':
     x = np.linspace(0, 2 * np.pi, 1001)
     sin2 = np.sin(x)
     cos2 = np.cos(x)
+    tan2 = np.tan(x)
+
+    tan2[tan2 > 3] = np.inf
+    tan2[tan2 < -3] = np.inf
 
     # Then, plot both series on the same axis
     plt.plot(x, sin2, 'r-', label='sin')
     plt.plot(x, cos2, 'b-', label='cos')
+    plt.plot(x, tan2, 'g-', label='tan')
     plt.xticks([0, np.pi/2, np.pi, (3*np.pi)/2, 2*np.pi], ['0', 'pi/2', 'pi', '3pi/2', '2pi'])
     plt.xlabel('Angle (in radians)')
     plt.ylabel('Value')
-    plt.title('Sine and cosine over one period')
+    plt.title('Sine, cosine, and tan over one period')
     plt.legend(loc=3)
+    plt.ylim(-3, 3)
+    plt.xlim(0, 2 * np.pi)
     plt.show()
